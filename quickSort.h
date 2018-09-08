6e6e75d3e0f5cdbac9aebc595ab10dc7aca6e247
Steven <steeletet@gmail.com>
2018-09-07 17:17:28 -0700

    Templatized quickSort function. Moved implementation to header file and deleted quicksort.cpp

--- quickSort.h
+++ quickSort.h
@@ -2,7 +2,61 @@
 #include <vector>
 using namespace std;
 
-void Allocate(vector<int>::iterator& first, 
-                vector<int>::iterator& last, vector<int>::iterator& pivot);
-void quickSort(vector<int>::iterator first, vector<int>::iterator last);
+template<typename T>
+void Allocate(T& first, T& last, T& pivot)
+{
+    pivot = first;
+    T before = first;
+    T after = first;
+    T current = first;
+
+    while (current <= last)
+    {
+        if (*current > *pivot)
+        {
+            after = current;
+            break;
+        }
+        else 
+            before = current;
+
+        ++current;
+    }
+
+    int dummy;
+    while (current <= last)
+    {
+        if (*current <= *pivot)
+        {
+            dummy = *after;
+            *after = *current;
+            *current = dummy;
+            ++before;
+            ++after;
+        }
+        if (current == last)
+            last = current;
+        ++current;
+    }
+
+    dummy = *pivot;
+    *pivot = *before;
+    *before = dummy;
+
+    first = pivot;
+    pivot = before;
+};
+
+template<typename T>
+void quickSort(T first, T last)
+{
+    T pivot;
+
+    Allocate(first, last, pivot);
+    if (pivot != first)
+        quickSort(first, pivot - 1);
+    if (pivot != last)
+        quickSort(pivot + 1, last);
+	   
+}
 
